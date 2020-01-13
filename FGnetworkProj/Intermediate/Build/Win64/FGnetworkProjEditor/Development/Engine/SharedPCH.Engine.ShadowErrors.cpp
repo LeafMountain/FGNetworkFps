0ef00b07b158a334e6b7d966fcbda5db9b2f4cce
Oscar Wiberg <Oscar@werbig.com>
2020-01-13 10:16:12 +0100

    Added unreal project

---
+++ FGnetworkProj/Intermediate/Build/Win64/FGnetworkProjEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/oscar.wiberg/Documents/FGNetworkFps/FGnetworkProj/Intermediate/Build/Win64/FGnetworkProjEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ FGnetworkProj/Source/FGnetworkProj/FGnetworkProjGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FGnetworkProjGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFGnetworkProjGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFGnetworkProjGameMode();
+};
+
+
+
